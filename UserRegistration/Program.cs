12f86a6f82a5c77a5b74c4a99d16d227031d2a32
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-19 15:32:28 +0530

    feat:Welcome to the user registration validation using c#

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the user registration validation using c#");
+        }
+    }
+}
